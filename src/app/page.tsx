55039a5ec6bd42c13a500edac6c94d2b31d4ae48
Omid Mohebbi <omidmohebbi@mac.home>
2024-10-26 15:28:03 +0200

    chage home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="d-flex flex-column justify-content-center align-items-center vh-100" >
             <div className="text-center w-75">
                 <div className=' h-100 w-100'>
-                   
+                   MAFIA
                 </div>
                 <div className='h-100 ' >
                     <a href="/join-game"><Button className='m-2  bg-dark' style={{ width: "250px" }}> Join with URL</Button></a>
@@ -14,19 +14,6 @@ const Home = () => {
                 </div>
             </div>
         </div>
-
-        {/* <div className="container vh-100 w-100 ">
-            <div className=' row h-100 align-items-center'>
-                <div className='col-9 h-100 '>
-                    <img src={MAFIA_LOGO} className='w-100'></img>
-                </div>
-
-                <div className=' col-3  text-center h-100 '>
-                    <Link to="/join-game"><Button className='mt-auto  bg-dark' style={{ width: "250px" }}> Join with URL</Button></Link>
-                    <Link to="/new-game "><Button className='m-b-auto ' style={{ width: "250px" }} > Create a new game </Button></Link>
-                </div>
-            </div>
-        </div> */}
     </div>)
 }
 
